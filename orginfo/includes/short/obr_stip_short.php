f91431f7ec2286ca0165f0317b50e20a4740bceb
Violetta Dekker <vikkerus666@gmail.com>
2019-09-10 17:10:00 +0600

    Update obr_stip_short.php

--- orginfo/includes/short/obr_stip_short.php
+++ orginfo/includes/short/obr_stip_short.php
@@ -1,32 +1,39 @@
 <?php $data = obr_stip_front(); ?>
 
 <div class="obr-front stip" itemprop="grant">
-	<?php if(isset($data['stipusl']) && !empty($data['stipusl'])) : ?>
 	<div class="info-block">
-		<div class="title">Условия предоставления стипендий</div>
-		<div class="content">
+		<div class="title">Условия предоставления стипендий</div>	
+		<?php if(isset($data['stipusl']) && !empty($data['stipusl'])) : ?>
+			<div class="content">
 			<?php echo $data['stipusl']?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['stip_obsh']) && !empty($data['stip_obsh'])) : ?>
 	<div class="info-block">
-		<div class="title">Общежитие, интернат (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="hostelInfo hostelInfoOvz">
-			<?php echo wp_unslash($data['stip_obsh'])?>
-		</div>
+		<div class="title">Общежитие, интернат (в том числе для лиц с ОВЗ)</div>		
+		<?php if(isset($data['stip_obsh']) && !empty($data['stip_obsh'])) : ?>
+			<div class="content" itemprop="hostelInfo hostelInfoOvz">
+			<?php echo wp_unslash($data['stip_obsh']);?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
+
 	
-	<?php if(isset($data['stip_kol']) && !empty($data['stip_kol'])) : ?>
 	<div class="info-block">
-		<div class="title">Количество жилых помещений в общежитии, интернате для иногородних обучающихся (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="hostelNum hostelNumOvz">
-			<?php echo wp_unslash($data['stip_kol'])?>
-		</div>
+		<div class="title">Количество жилых помещений в общежитии, интернате для иногородних обучающихся (в том числе для лиц с ОВЗ)</div>	
+		<?php if(isset($data['stip_kol']) && !empty($data['stip_kol'])) : ?>
+			<div class="content" itemprop="hostelNum hostelNumOvz">
+			<?php echo wp_unslash($data['stip_kol']);?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
 	</div>
-	<?php endif;?>
 	
 	<?php if(isset($data['stip_obsh_id']) && !empty($data['stip_obsh_id'])) : ?>
 	<div class="info-block">
@@ -35,23 +42,32 @@
 			<a itemprop="localActObSt" href="<?php echo (isset($data['stip_obsh_url']) && !empty($data['stip_obsh_url'])) ? $data['stip_obsh_url'] : ''?>" title="Размер платы" target="_blank"><?php echo (isset($data['stip_obsh_name']) && !empty($data['stip_obsh_name'])) ? wp_unslash($data['stip_obsh_name']) : 'Акт, регламентирующий размер платы за пользование жилым помещением и коммунальные услуги в общежитии'?></a>
 		</div>
 	</div>
-	<?php endif;?>
-	
-	<?php if(isset($data['stip_other']) && !empty($data['stip_other'])) : ?>
+	<?php else:?>
 	<div class="info-block">
-		<div class="title">Иные виды материальной поддержки обучающихся</div>
-		<div class="content" itemprop="support">
-			<?php echo wp_unslash($data['stip_other'])?>
-		</div>
+		<div class="title">Копия локального нормативного акта, регламентирующего размер платы за пользование жилым помещением и коммунальные услуги в общежитии</div>
+		<div class="none-data">нет данных</div>
 	</div>
 	<?php endif;?>
 	
-	<?php if(isset($data['stiptrud']) && !empty($data['stiptrud'])) : ?>
 	<div class="info-block">
-		<div class="title">Трудоустройство выпускников</div>
-		<div class="content" itemprop = "graduateJob">
-			<?php echo $data['stiptrud']?>
-		</div>
+		<div class="title">Иные виды материальной поддержки обучающихся</div>	
+		<?php if(isset($data['stip_other']) && !empty($data['stip_other'])) : ?>
+			<div class="content" itemprop="support">
+			<?php echo wp_unslash($data['stip_other']);?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
+	
+	<div class="info-block">
+		<div class="title">Трудоустройство выпускников</div>		
+		<?php if(isset($data['stiptrud']) && !empty($data['stiptrud'])) : ?>
+			<div class="content" itemprop = "graduateJob">
+			<?php echo $data['stiptrud'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
 </div>
