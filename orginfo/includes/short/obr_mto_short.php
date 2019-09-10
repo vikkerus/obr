16df2dd45e6b2458163bdf42df0cb3d0522db694
Violetta Dekker <vikkerus666@gmail.com>
2019-09-10 17:09:52 +0600

    Update obr_mto_short.php

--- orginfo/includes/short/obr_mto_short.php
+++ orginfo/includes/short/obr_mto_short.php
@@ -10,77 +10,96 @@
 	</div>
 	<?php endif;?>
 	
-	<?php if(isset($data['mat_uch']) && !empty($data['mat_uch'])) : ?>
+	
 	<div class="info-block">
-		<div class="title">Оборудованные учебные кабинеты (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="purposeCab purposeCabOvz">
+		<div class="title">Оборудованные учебные кабинеты (в том числе для лиц с ОВЗ)</div>		
+		<?php if(isset($data['mat_uch']) && !empty($data['mat_uch'])) : ?>
+			<div class="content" itemprop="purposeCab purposeCabOvz">
 			<?php echo wp_unslash($data['mat_uch'])?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
-	
-	<?php if(isset($data['mat_pract']) && !empty($data['mat_pract'])) : ?>
+
+
 	<div class="info-block">
-		<div class="title">Объекты для проведения практических занятий (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="purposePrac purposePracOvz">
+		<div class="title">Объекты для проведения практических занятий (в том числе для лиц с ОВЗ)</div>	
+		<?php if(isset($data['mat_pract']) && !empty($data['mat_pract'])) : ?>
+			<div class="content" itemprop="purposePrac purposePracOvz">
 			<?php echo wp_unslash($data['mat_pract'])?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
+
 	
-	<?php if(isset($data['mat_bibl']) && !empty($data['mat_bibl'])) : ?>
 	<div class="info-block">
 		<div class="title">Библиотеки (в том числе для лиц с ОВЗ)</div>
 		<div class="content" itemprop="purposeLibr purposeLibrOvz">
+			<?php if(isset($data['mat_bibl']) && !empty($data['mat_bibl'])) : ?>
 			<?php echo wp_unslash($data['mat_bibl'])?>
+			<?php else:?>
+				<div class="none-data">нет данных</div>
+			<?php endif;?>
 		</div>
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['mat_sport']) && !empty($data['mat_sport'])) : ?>
 	<div class="info-block">
-		<div class="title">Объекты спорта (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="purposeSport purposeSportOvz">
+		<div class="title">Объекты спорта (в том числе для лиц с ОВЗ)</div>	
+		<?php if(isset($data['mat_sport']) && !empty($data['mat_sport'])) : ?>
+			<div class="content" itemprop="purposeSport purposeSportOvz">
 			<?php echo wp_unslash($data['mat_sport'])?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['mat_vos']) && !empty($data['mat_vos'])) : ?>
 	<div class="info-block">
-		<div class="title">Средства обучения и воспитания (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="purposeFacil purposeFacilOvz">
+		<div class="title">Средства обучения и воспитания (в том числе для лиц с ОВЗ)</div>	
+		<?php if(isset($data['mat_vos']) && !empty($data['mat_vos'])) : ?>
+			<div class="content" itemprop="purposeFacil purposeFacilOvz">
 			<?php echo wp_unslash($data['mat_vos'])?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['mat_ohr']) && !empty($data['mat_ohr'])) : ?>
 	<div class="info-block">
 		<div class="title">Условия питания и охраны здоровья обучающихся (в том числе для лиц с ОВЗ)</div>
 		<div class="content" itemprop="mealsDocLink meals mealsOvz">
+			<?php if(isset($data['mat_ohr']) && !empty($data['mat_ohr'])) : ?>
 			<?php echo wp_unslash($data['mat_ohr'])?>
+			<?php else:?>
+				<div class="none-data">нет данных</div>
+			<?php endif;?>
 		</div>
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['mat_its']) && !empty($data['mat_its'])) : ?>
 	<div class="info-block">
-		<div class="title">Доступ к информационным системам и информационно-телекоммуникационным сетям (в том числе для лиц с ОВЗ)</div>
-		<div class="content" itemprop="comNet comNetOvz">
+		<div class="title">Доступ к информационным системам и информационно-телекоммуникационным сетям (в том числе для лиц с ОВЗ)</div>	
+		<?php if(isset($data['mat_its']) && !empty($data['mat_its'])) : ?>
+			<div class="content" itemprop="comNet comNetOvz">
 			<?php echo wp_unslash($data['mat_its'])?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
 	
-	<?php if(isset($data['matres']) && !empty($data['matres'])) : ?>
 	<div class="info-block">
-		<div class="title">Электронные образовательные ресурсы</div>
-		<div class="content" itemprop="erList erListOvz">
+		<div class="title">Электронные образовательные ресурсы</div>	
+		<?php if(isset($data['matres']) && !empty($data['matres'])) : ?>
+			<div class="content" itemprop="erList erListOvz">
 			<?php echo $data['matres']?>
-		</div>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
 	</div>
-	<?php endif;?>
 </div>
 
 
