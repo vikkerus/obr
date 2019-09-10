0cad33e0e96069e390e74d012636ce0b1920020a
Violetta Dekker <vikkerus666@gmail.com>
2019-09-10 17:10:05 +0600

    Update obr_vac_short.php

--- orginfo/includes/short/obr_vac_short.php
+++ orginfo/includes/short/obr_vac_short.php
@@ -1,12 +1,14 @@
 <?php $data = obr_vac_front(); ?>
 
-<div class="obr-front vac" itemprop="vacant">
-	<?php if(isset($data['vacinfo']) && !empty($data['vacinfo'])) : ?>
+<div class="obr-front vac">
 	<div class="info-block">
-		<div class="title">Вакантные места для приема (перевода)</div>
-		<div class="content">
-			<?php echo $data['vacinfo']?>
-		</div>
+		<div class="title">Вакантные места для приема (перевода)</div>		
+		<?php if(isset($data['vacinfo']) && !empty($data['vacinfo'])) : ?>
+			<div class="content" itemprop="vacant">
+			<?php echo $data['vacinfo'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
 	</div>
-	<?php endif;?>
 </div>
