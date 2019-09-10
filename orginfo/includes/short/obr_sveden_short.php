75f7e6f48565d6224aeadd1d58807f780dbdcb7a
Violetta Dekker <vikkerus666@gmail.com>
2019-09-10 11:06:31 +0600

    Create obr_sveden_short.php

---
+++ orginfo/includes/short/obr_sveden_short.php
@@ -0,0 +1,37 @@
+<div class="obr-front sveden">
+	<ul>
+		<li>
+			<a href="<?php echo site_url('/sveden/common');?>" title="Основные сведения">Основные сведения</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/struct');?>" title="Структура и органы управления образовательной организацией">Структура и органы управления образовательной организацией</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/document');?>" title="Документы">Документы</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/education');?>" title="Образование">Образование</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/edustandarts');?>" title="Образовательные стандарты">Образовательные стандарты</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/employees');?>" title="Руководство. Педагогический (научно-педагогический) состав">Руководство. Педагогический (научно-педагогический) состав</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/objects');?>" title="Материально-техническое обеспечение и оснащенность образовательного процесса">Материально-техническое обеспечение и оснащенность образовательного процесса</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/grants');?>" title="Стипендии и иные виды материальной поддержки">Стипендии и иные виды материальной поддержки</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/paid_edu');?>" title="Платные образовательные услуги">Платные образовательные услуги</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/budget');?>" title="Финансово-хозяйственная деятельность">Финансово-хозяйственная деятельность</a>
+		</li>
+		<li>
+			<a href="<?php echo site_url('/sveden/vacant');?>" title="Вакантные места для приема (перевода)">Вакантные места для приема (перевода)</a>
+		</li>
+	</ul>
+</div>
