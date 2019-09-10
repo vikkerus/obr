c5ac15f901d35755b76b47002dfcaa2c1ba9c3bc
Violetta Dekker <vikkerus666@gmail.com>
2019-09-10 11:06:36 +0600

    Create obr_stand_short.php

---
+++ orginfo/includes/short/obr_stand_short.php
@@ -0,0 +1,23 @@
+<?php 
+
+	$data = obr_stand_front(); 
+
+	//$link_error = '<span class="link_error">(ссылка недействительна) </span>';
+
+?>
+
+<div class="obr-front stand">
+	<?php if(is_array($data)) : ?>
+	<div class="link-title">
+		Копии федеральных государственных образовательных стандартов:
+	</div>
+	<?php foreach($data as $key => $val):?>
+		<?php if(isset($val['link']) && !empty($val['link'])) : ?>
+		<div class="info-link">
+			<?php //echo (obr_get_status($val['link']) ? '' : $link_error)?>
+			<a itemprop="eduStandartDoc" href="<?php echo $val['link']?>" title="Образовательный стандарт" target="_blank"><?php echo (isset($val['name']) && !empty($val['name'])) ? wp_unslash($val['name']) : 'Копия федерального государственного образовательного стандарта'?></a>
+		</div>
+		<?php endif;?>						
+	<?php endforeach;?>
+	<?php endif;?>
+</div>
