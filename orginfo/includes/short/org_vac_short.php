0eb2089049718e86e6fd8d82ae2bdff3eae79128
Violetta Dekker <vikkerus666@gmail.com>
2019-09-11 10:40:01 +0600

    Create org_vac_short.php

---
+++ orginfo/includes/short/org_vac_short.php
@@ -0,0 +1,14 @@
+<?php $data = org_vac_front(); ?>
+
+<div class="org-front vac">
+	<div class="info-block">
+		<div class="title">Вакантные места для приема (перевода)</div>		
+		<?php if(isset($data['vacinfo']) && !empty($data['vacinfo'])) : ?>
+			<div class="content" itemprop="vacant">
+			<?php echo $data['vacinfo'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>	
+	</div>
+</div>
