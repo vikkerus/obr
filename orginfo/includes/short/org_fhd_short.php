7c7feaded7f3d0178cc6e74b485c9629462968aa
Violetta Dekker <vikkerus666@gmail.com>
2019-10-10 14:35:48 +0600

    Update org_fhd_short.php

--- orginfo/includes/short/org_fhd_short.php
+++ orginfo/includes/short/org_fhd_short.php
@@ -1,28 +1,73 @@
 <?php $data = org_fhd_front(); ?>
 
-<div class="org-front fhd" itemprop="volume" itemscope itemtype="http://obrnadzor.gov.ru/microformats/Volume">
+<div class="org-front fhd" itemprop="volume">
 
 	<div class="info-block">
-		<div class="title">Объем образовательной деятельности, финансовое обеспечение которой осуществляется за счет бюджетных ассигнований федерального бюджета, бюджетов субъектов Российской Федерации, местных бюджетов, по договорам об образовании за счет средств физических и (или) юридических лиц</div>		
-		<?php if(isset($data['finob']) && !empty($data['finob'])) : ?>
-			<div class="content" itemprop="finBFVolume finBRVolume finBMVolume finPVolume">
-			<?php echo $data['finob'];?>
+		<div class="title">Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>бюджетных ассигнований федерального бюджета</u></div>		
+		<?php if(isset($data['finfb']) && !empty($data['finfb'])) : ?>
+			<div class="content" itemprop="finBFVolume">
+			<?php echo $data['finfb'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
+	
+	<div class="info-block">
+		<div class="title">Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>бюджетов субъектов Российской Федерации</u></div>		
+		<?php if(isset($data['finbr']) && !empty($data['finbr'])) : ?>
+			<div class="content" itemprop="finBRVolume">
+			<?php echo $data['finbr'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
+	
+	<div class="info-block">
+		<div class="title">Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>местных бюджетов</u></div>		
+		<?php if(isset($data['finmb']) && !empty($data['finmb'])) : ?>
+			<div class="content" itemprop="finBMVolume">
+			<?php echo $data['finmb'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
+	
+	<div class="info-block">
+		<div class="title">Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется по договорам об образовании за счёт <u>средств физических и (или) юридических лиц</u></div>		
+		<?php if(isset($data['finfiz']) && !empty($data['finfiz'])) : ?>
+			<div class="content" itemprop="finPVolume">
+			<?php echo $data['finfiz'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
+	
+	<div class="info-block">
+		<div class="title">Сведения о поступлении и расходовании финансовых и материальных средств</div>		
+		<?php if(isset($data['finpo']) && !empty($data['finpo'])) : ?>
+			<div class="content" itemprop="finRec">
+			<?php echo $data['finpo'];?>
+			</div>
+		<?php else:?>
+			<div class="none-data">нет данных</div>
+		<?php endif;?>		
+	</div>
+	
+	<div class="info-block">
+		<div class="title">Планы финансово-хозяйственной деятельности</div>		
+		<?php if(isset($data['finplan']) && !empty($data['finplan'])) : ?>
+			<div class="content" itemprop="planFinRec">
+			<?php echo $data['finplan'];?>
 			</div>
 		<?php else:?>
 			<div class="none-data">нет данных</div>
 		<?php endif;?>		
 	</div>
 
-	
-	<div class="info-block">
-		<div class="title">Поступление и расходовании финансовых и материальных средств</div>		
-		<?php if(isset($data['finpo']) && !empty($data['finpo'])) : ?>
-			<div class="content" itemprop="finRec" itemscope itemtype="http://obrnadzor.gov.ru/microformats/FinRec">
-			<?php echo $data['finpo'];?>
-			</div>
-		<?php else:?>
-			<div class="none-data">нет данных</div>
-		<?php endif;?>	
-	</div>
+
 
 </div>
