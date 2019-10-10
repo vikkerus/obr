dc3e67a247127572c5756ddaa9c44900fc6b2d22
Violetta Dekker <vikkerus666@gmail.com>
2019-10-10 14:35:44 +0600

    Update org_finance_page.php

--- orginfo/includes/org_finance_page.php
+++ orginfo/includes/org_finance_page.php
@@ -23,16 +23,17 @@ if(isset($data['action'])) : ?>
 		?>
 		
 		<div class="finance">
+			
 			<div class="input-block editor-block">
-				<label>Информация об объеме образовательной деятельности, финансовое обеспечение которой осуществляется за счет бюджетных ассигнований федерального бюджета, бюджетов субъектов Российской Федерации, местных бюджетов, по договорам об образовании за счет средств физических и (или) юридических лиц</label>
+				<label>Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>бюджетных ассигнований федерального бюджета</u></label>
 				<?php 
 					wp_editor(
-						(isset($field['finob']) ? $field['finob'] : ''), 
-						'finob', 
+						(isset($field['finfb']) ? $field['finfb'] : ''), 
+						'finfb', 
 						array(
 							'wpautop'       => 0,
 							'media_buttons' => 1,
-							'textarea_name' => 'finob',
+							'textarea_name' => 'finfb',
 							'tabindex'      => null,
 							'textarea_rows' => 10,
 							'teeny'         => 0,
@@ -44,8 +45,75 @@ if(isset($data['action'])) : ?>
 					);
 				?>
 			</div>
+			
 			<div class="input-block editor-block">
-				<label>Информация о поступлении и расходовании финансовых и материальных средств</label>
+				<label>Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>бюджетов субъектов Российской Федерации</u></label>
+				<?php 
+					wp_editor(
+						(isset($field['finbr']) ? $field['finbr'] : ''), 
+						'finbr', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'finbr',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+			
+			<div class="input-block editor-block">
+				<label>Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется за счёт <u>местных бюджетов</u></label>
+				<?php 
+					wp_editor(
+						(isset($field['finmb']) ? $field['finmb'] : ''), 
+						'finmb', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'finmb',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+			
+			<div class="input-block editor-block">
+				<label>Сведения об объёме образовательной деятельности, финансовое обеспечение которой осуществляется по договорам об образовании за счёт <u>средств физических и (или) юридических лиц</u></label>
+				<?php 
+					wp_editor(
+						(isset($field['finfiz']) ? $field['finfiz'] : ''), 
+						'finfiz', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'finfiz',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+			
+			<div class="input-block editor-block">
+				<label>Сведения о поступлении и расходовании финансовых и материальных средств</label>
 				<?php 
 					wp_editor(
 						(isset($field['finpo']) ? $field['finpo'] : ''), 
@@ -65,6 +133,29 @@ if(isset($data['action'])) : ?>
 					);
 				?>
 			</div>
+			
+			<div class="input-block editor-block">
+				<label>Ссылки на копии планов финансово-хозяйственной деятельности</label>
+				<?php 
+					wp_editor(
+						(isset($field['finplan']) ? $field['finplan'] : ''), 
+						'finplan', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'finplan',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+			
 			<div class="input-block">
 				<button class="form_btn" name="fin_btn" type="submit">Сохранить</button>
 			</div>
