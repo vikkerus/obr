5f792e720a716edd940ee5d63100b4b77fcb63ef
vikkerus <49053629+vikkerus@users.noreply.github.com>
2020-12-11 10:12:46 +0600

    Update org_stipend_page.php

--- orginfo/includes/org_stipend_page.php
+++ orginfo/includes/org_stipend_page.php
@@ -33,45 +33,16 @@ if(isset($data['action'])) : ?>
 				<a href="javascript:;" class="doc_remove link_btn <?php echo (!empty($field['stip_doc_url']) && !empty($field['stip_doc_id']) ? '' : 'block-hidden')?>">Очистить документ</a>
 			</div>
             
-            
-            
-            
-            
-            
-            
-            
-            
-			
-			<div class="input-block">
-				<label>Информация о наличии общежития, интерната (в том числе для лиц с ОВЗ)</label>
-				<textarea name="stip_obsh" class="field"><?php echo (isset($field['stip_obsh']) ? wp_unslash($field['stip_obsh']) : '')?></textarea>
-			</div>
-			<div class="input-block">
-				<label>Информация о количестве жилых помещений в общежитии, интернате для иногородних обучающихся (в том числе для лиц с ОВЗ)</label>
-				<textarea name="stip_kol" class="field"><?php echo (isset($field['stip_kol']) ? wp_unslash($field['stip_kol']) : '')?></textarea>
-			</div>
-			<div class="input-block input_uploader" id="stip_input_1">
-				<label>Копия локального нормативного акта, регламентирующего размер платы за пользование жилым помещением и коммунальные услуги в общежитии</label>
-				<input placeholder="Заголовок документа" class="doc_name" type="text" name="stip_obsh_name" value="<?php echo (isset($field['stip_obsh_name']) ? wp_unslash(htmlspecialchars($field['stip_obsh_name'])) : '')?>">				
-				<input class="doc_id block-hidden" type="text" name="stip_obsh_id" value="<?php echo (isset($field['stip_obsh_id']) ? $field['stip_obsh_id'] : '')?>">
-				<input class="doc_url" readonly="readonly" type="text" name="stip_obsh_url" value="<?php echo (isset($field['stip_obsh_url']) ? $field['stip_obsh_url'] : '')?>">
-				<a href="javascript:;" class="doc_upload link_btn <?php echo (!empty($field['stip_obsh_url']) && !empty($field['stip_obsh_id']) ? 'block-hidden' : '')?>">Загрузить документ</a>
-				<a href="javascript:;" class="doc_remove link_btn <?php echo (!empty($field['stip_obsh_url']) && !empty($field['stip_obsh_id']) ? '' : 'block-hidden')?>">Очистить документ</a>
-			</div>
-			<div class="input-block ">
-				<label>Информация об иных видах материальной поддержки обучающихся</label>
-				<textarea name="stip_other" class="field"><?php echo (isset($field['stip_other']) ? wp_unslash($field['stip_other']) : '')?></textarea>
-			</div>
-			<div class="input-block editor-block">
-				<label>Информация о трудоустройстве выпускников (документы, таблица или текст)</label>
+            <div class="input-block editor-block">
+				<label>Информация о предоставлении стипендии обучающимся</label>
 				<?php 
 					wp_editor(
-						(isset($field['stiptrud']) ? $field['stiptrud'] : ''), 
-						'stiptrud', 
+						(isset($field['grant']) ? $field['grant'] : ''), 
+						'grant', 
 						array(
 							'wpautop'       => 0,
 							'media_buttons' => 1,
-							'textarea_name' => 'stiptrud',
+							'textarea_name' => 'grant',
 							'tabindex'      => null,
 							'textarea_rows' => 10,
 							'teeny'         => 0,
@@ -83,6 +54,155 @@ if(isset($data['action'])) : ?>
 					);
 				?>
 			</div>
+            
+            <div class="input-block editor-block">
+				<label>Информация о мерах социальной поддержки обучающихся</label>
+				<?php 
+					wp_editor(
+						(isset($field['support']) ? $field['support'] : ''), 
+						'support', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'support',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+			
+			<div class="input-block">
+				<label>Количество общежитий</label>
+				<textarea name="hostelInfo" class="field"><?php echo (isset($field['hostelInfo']) ? wp_unslash($field['hostelInfo']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Количество интернатов</label>
+				<textarea name="interInfo" class="field"><?php echo (isset($field['interInfo']) ? wp_unslash($field['interInfo']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Общая площадь общежитий</label>
+				<textarea name="hostelTS" class="field"><?php echo (isset($field['hostelTS']) ? wp_unslash($field['hostelTS']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Общая площадь интернатов</label>
+				<textarea name="interTS" class="field"><?php echo (isset($field['interTS']) ? wp_unslash($field['interTS']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Жилая площадь общежитий</label>
+				<textarea name="hostelLS" class="field"><?php echo (isset($field['hostelLS']) ? wp_unslash($field['hostelLS']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Жилая площадь интернатов</label>
+				<textarea name="interLS" class="field"><?php echo (isset($field['interLS']) ? wp_unslash($field['interLS']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Количество мест в общежитиях</label>
+				<textarea name="hostelNum" class="field"><?php echo (isset($field['hostelNum']) ? wp_unslash($field['hostelNum']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Количество мест в интернатах</label>
+				<textarea name="interNum" class="field"><?php echo (isset($field['interNum']) ? wp_unslash($field['interNum']) : '')?></textarea>
+			</div>
+            <div class="input-block editor-block">
+				<label>Обеспеченность общежитий 100% мягким и жестким инвентарем по установленным стандартным нормам</label>
+				<?php 
+					wp_editor(
+						(isset($field['hostelInv']) ? $field['hostelInv'] : ''), 
+						'hostelInv', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'hostelInv',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+            <div class="input-block editor-block">
+				<label>Обеспеченность интернатов 100% мягким и жестким инвентарем по установленным стандартным нормам</label>
+				<?php 
+					wp_editor(
+						(isset($field['interInv']) ? $field['interInv'] : ''), 
+						'interInv', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'interInv',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+            <div class="input-block">
+				<label>Наличие питания (да/нет) в общежитиях</label>
+				<textarea name="hostelFd" class="field"><?php echo (isset($field['hostelFd']) ? wp_unslash($field['hostelFd']) : '')?></textarea>
+			</div>
+            <div class="input-block">
+				<label>Наличие питания (да/нет) в интернатах</label>
+				<textarea name="interFd" class="field"><?php echo (isset($field['interFd']) ? wp_unslash($field['interFd']) : '')?></textarea>
+			</div>
+            <div class="input-block editor-block">
+				<label>Информация о формировании платы за проживание в общежитии</label>
+				<?php 
+					wp_editor(
+						(isset($field['localAct']) ? $field['localAct'] : ''), 
+						'localAct', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'localAct',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+            <div class="input-block editor-block">
+				<label>Информация о трудоустройстве выпускников</label>
+				<?php 
+					wp_editor(
+						(isset($field['graduateJob']) ? $field['graduateJob'] : ''), 
+						'graduateJob', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'graduateJob',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+            
+            
 			<div class="input-block">
 				<button class="form_btn" name="stip_btn" type="submit">Сохранить</button>
 			</div>
