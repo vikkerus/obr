5de8b670323dc8eff6c6873d7015d8da7d1befb2
vikkerus <49053629+vikkerus@users.noreply.github.com>
2020-12-11 10:12:49 +0600

    Update org_paid_short.php

--- orginfo/includes/short/org_paid_short.php
+++ orginfo/includes/short/org_paid_short.php
@@ -8,27 +8,28 @@
 
 <div class="org-front paid">
 	<div class="info-block">
-		<?php if(isset($data['plat']) && is_array($data['plat'])) : $plat = $data['plat']; ?>
-		<div class="link-title">
-			Документы о порядке оказания платных образовательных услуг, в том числе образец договора об оказании платных образовательных услуг, документ об утверждении стоимости обучения по каждой образовательной программе:
-		</div>
-		<div id="paid_links">
-		<?php foreach($plat as $key => $val):?>
-			<?php if((isset($val['docs_plat_link']) && !empty($val['docs_plat_link'])) || (isset($val['docs_plat_url']) && !empty($val['docs_plat_url']))) : ?>
-			<div class="doc-block">
-				<div class="content">
-					<?php //echo (org_get_status($val['docs_plat_link']) ? '' : $link_error)?>
-					<a itemprop= "paidEdu" href="<?php echo ((isset($val['docs_plat_link'])) && (!empty($val['docs_plat_link'])) ? $val['docs_plat_link'] : $val['docs_plat_url'])?>" title="Порядок оказания платных образовательных услуг" target="_blank"><?php echo (isset($val['docs_plat_name']) && !empty($val['docs_plat_name'])) ? wp_unslash($val['docs_plat_name']) : 'Порядок оказания платных образовательных услуг'?></a>
-				</div>
-			</div>
-			<?php endif;?>	
-		<?php endforeach;?>
-		</div>
-		<?php else:?>
-		<div class="link-title">
-			Документы о порядке оказания платных образовательных услуг, в том числе образец договора об оказании платных образовательных услуг, документ об утверждении стоимости обучения по каждой образовательной программе:
-		</div>
-		<div class="none-data">нет данных</div>
-		<?php endif;?>
+		<div class="info-block">
+            <div class="title">Информация о порядке оказания платных образовательных услуг</div>		
+            <?php if(isset($data['paidEdu']) && !empty($data['paidEdu'])) : ?>
+                <div class="content" itemprop = "paidEdu">
+                <?php echo $data['paidEdu'];?>
+                </div>
+            <?php else:?>
+                <div class="none-data">нет данных</div>
+            <?php endif;?>		
+        </div>
+	</div>
+    
+    <div class="info-block">
+		<div class="info-block">
+            <div class="title">Документ об установлении размера платы, взимаемой с родителей (законных представителей) за присмотр и уход за детьми, осваивающими образовательные программы дошкольного образования в организациях, осуществляющих образовательную деятельность, за содержание детей в образовательной организации, реализующей образовательные программы начального общего, основного общего или среднего общего образования, если в такой образовательной организации созданы условия для проживания обучающихся в интернате, либо за осуществление присмотра и ухода за детьми в группах продленного дня в образовательной организации, реализующей образовательные программы начального общего, основного общего или среднего общего образования</div>		
+            <?php if(isset($data['paidParents']) && !empty($data['paidParents'])) : ?>
+                <div class="content" itemprop = "paidParents">
+                <?php echo $data['paidParents'];?>
+                </div>
+            <?php else:?>
+                <div class="none-data">нет данных</div>
+            <?php endif;?>		
+        </div>
 	</div>
 </div>
