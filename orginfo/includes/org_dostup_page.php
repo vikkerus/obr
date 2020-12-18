d69d55a2a1e6ebd8d9f4e9241365fd004a22adb9
vikkerus <49053629+vikkerus@users.noreply.github.com>
2020-12-18 11:52:53 +0600

    Update org_dostup_page.php

--- orginfo/includes/org_dostup_page.php
+++ orginfo/includes/org_dostup_page.php
@@ -200,6 +200,63 @@ if(isset($data['action'])) : ?>
 				?>
 			</div>
             
+            <div class="input-block">
+				<label>Информация о приспособленных средствах обучения и воспитания</label>
+				<textarea name="facil" class="field"><?php echo (isset($field['facil']) ? wp_unslash($field['facil']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block">
+				<label>Информация об обеспечении беспрепятственного доступа в здания образовательной организации</label>
+				<textarea name="ovz" class="field"><?php echo (isset($field['ovz']) ? wp_unslash($field['ovz']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block">
+				<label>Информация о доступе к приспособленным информационным системам и информационно- телекоммуникационным сетям</label>
+				<textarea name="net" class="field"><?php echo (isset($field['net']) ? wp_unslash($field['net']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block editor-block education_obr">
+				<label>Информация о приспособленных электронных образовательных ресурсах, к которым обеспечивается доступ</label>
+				<br>
+				<?php 
+					wp_editor(
+						(isset($field['eorlink']) ? $field['eorlink'] : ''), 
+						'eorlink', 
+						array(
+							'wpautop'       => 0,
+							'media_buttons' => 1,
+							'textarea_name' => 'eorlink',
+							'tabindex'      => null,
+							'textarea_rows' => 10,
+							'teeny'         => 0,
+							'dfw'           => 0,
+							'tinymce'       => 1,
+							'quicktags'     => 1,
+							'drag_drop_upload' => true
+						)
+					);
+				?>
+			</div>
+            
+            <div class="input-block">
+				<label>Информация о наличии специальных технических средств обучения коллективного и индивидуального пользования</label>
+				<textarea name="tech" class="field"><?php echo (isset($field['tech']) ? wp_unslash($field['tech']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block">
+				<label>Инфомация о наличии условий для беспрепятственного доступа в общежитии, интернате</label>
+				<textarea name="hostel" class="field"><?php echo (isset($field['hostel']) ? wp_unslash($field['hostel']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block">
+				<label>Количество жилых помещений в общежитии, приспособленных для использования инвалидами и лицами с ограниченными возможностями здоровья</label>
+				<textarea name="hostelnum" class="field"><?php echo (isset($field['hostelnum']) ? wp_unslash($field['hostelnum']) : '')?></textarea>
+			</div>
+            
+            <div class="input-block">
+				<label>Количество жилых помещений в интернате, приспособленных для использования инвалидами и лицами с ограниченными возможностями здоровья</label>
+				<textarea name="internum" class="field"><?php echo (isset($field['internum']) ? wp_unslash($field['internum']) : '')?></textarea>
+			</div>
             
 		</div>
 		<div class="input-block">
