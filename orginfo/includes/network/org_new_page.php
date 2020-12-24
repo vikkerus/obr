ec6cf386c84d13bfe16b3a3cce96a035cfb94804
vikkerus <49053629+vikkerus@users.noreply.github.com>
2020-12-24 12:51:09 +0600

    Update org_new_page.php

--- orginfo/includes/network/org_new_page.php
+++ orginfo/includes/network/org_new_page.php
@@ -1,2 +1,26 @@
-<?php $data = check_new_columns(); var_dump($data);?>
+<?php $data = check_new_columns(); echo '<pre>'; var_dump($data); echo '</pre>';?>
+
+<?php
+
+if(!empty($data) && is_array($data))
+{    
+    if(!empty($data['dostup']) && is_array($data['dostup']))
+    {
+        // проверим, есть ли в массиве значений Доступная среда нулевые значения
+        $dostup = in_array('0',$data['dostup'], true);
+        
+        // если есть, то выведем соответствующее сообщение
+        if($dostup === true)
+        {
+            echo 'Раздел "Доступная среда" присутствует не на всех сайтах. <b>Требуется обновление!</b>';
+        }
+        else
+        {
+            echo 'Раздел "Доступная среда" присутствует на всех сайтах.';
+        }
+    }
+}
+
+?>
+
 
